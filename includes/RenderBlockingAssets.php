14b95ec4f4dfec7509ac0aa0b551b4f32821617c
Peter Li <lihaohong6@gmail.com>
2025-11-14 23:03:39 -0500

    Add page to specify the list of render-blocking assets

--- includes/RenderBlockingAssets.php
+++ includes/RenderBlockingAssets.php
@@ -3,37 +3,100 @@
 namespace MediaWiki\Extension\RenderBlocking;
 
 use MediaWiki\Context\RequestContext;
+use MediaWiki\MediaWikiServices;
 use MediaWiki\Page\Article;
 use MediaWiki\Revision\RevisionRecord;
 use MediaWiki\Title\Title;
 use Wikimedia\Minify\CSSMin;
 use Wikimedia\Minify\JavaScriptMinifier;
+use Wikimedia\ObjectCache\WANObjectCache;
 
 class RenderBlockingAssets {
 
 	private const PAGE_PREFIX = 'MediaWiki:Renderblocking';
+	private static ?WANObjectCache $cache = null;
+
+	/**
+	 * Retrieves an array of page names listed on a wiki page.
+	 * Page titles must start with a * and omit the MediaWiki
+	 * namespace prefix.
+	 *
+	 * @param string $pageName Name of the wiki page
+	 *
+	 * @return array
+	 */
+	static function getPageList( string $pageName ): array {
+		$content = self::getPageContent( $pageName );
+		if ( !$content ) {
+			return [];
+		}
+		$result = [];
+		foreach ( explode( "\n", $content ) as $line ) {
+			if ( !str_starts_with( $line, '*' ) ) {
+				continue;
+			}
+			$pageName = trim( substr( $line, 1 ) );
+
+			if ( stripos( $pageName, 'MediaWiki:' ) === 0 ) {
+				$result[] = $pageName;
+			} else {
+				$result[] = "MediaWiki:" . $pageName;
+			}
+		}
+
+		return $result;
+	}
+
+	static function filterPageList( array $pageList, AssetType $assetType ): array {
+		$result = [];
+		foreach ( $pageList as $pageTitle ) {
+			$extensionIndex = strlen( $pageTitle ) - strlen( $assetType->value );
+			if ( stripos( $pageTitle, $assetType->value ) === $extensionIndex ) {
+				$result[] = $pageTitle;
+			}
+		}
+
+		return $result;
+	}
+
 
 	static function getAssetPageTitles( ?string $skinName, AssetType $assetType ): array {
-		$result = [
-			self::PAGE_PREFIX . ".$assetType->value",
+		$prefixList = [
+			self::PAGE_PREFIX
 		];
 		if ( $skinName ) {
-			$result[] = self::PAGE_PREFIX . "-$skinName.$assetType->value";
+			$prefixList[] = self::PAGE_PREFIX . "-$skinName";
+		}
+		$result = [];
+		foreach ( $prefixList as $prefix ) {
+			$result [] = "$prefix.$assetType->value";
+			$extraPages = self::getPageList( $prefix . "-pages" );
+			$extraPages = self::filterPageList( $extraPages, $assetType );
+			foreach ( $extraPages as $pageTitle ) {
+				$result[] = $pageTitle;
+			}
 		}
 
 		return $result;
 	}
 
 	static function getAssets( ?string $skinName, AssetType $assetType ): array {
-		$result = [];
-		foreach ( self::getAssetPageTitles( $skinName, $assetType ) as $pageTitle ) {
-			$content = self::getPageContent( $pageTitle );
-			if ( $content ) {
-				$result[$pageTitle] = $content;
-			}
+		if ( self::$cache === null ) {
+			self::$cache = MediaWikiServices::getInstance()->getMainWANObjectCache();
 		}
+		$cacheKey = "ext-renderblocking-$skinName-$assetType->value";
 
-		return $result;
+		return self::$cache->getWithSetCallback( $cacheKey, 600, function () use ( $skinName, $assetType ) {
+			$result = [];
+			foreach ( self::getAssetPageTitles( $skinName, $assetType ) as $pageTitle ) {
+				$content = self::getPageContent( $pageTitle );
+				if ( $content ) {
+					$result[$pageTitle] = $content;
+				}
+			}
+
+			return $result;
+		}, [ "lockTSE" => 10 ] );
 	}
 
 	static function minifyAssets( array $assets, AssetType $assetType ): string {
